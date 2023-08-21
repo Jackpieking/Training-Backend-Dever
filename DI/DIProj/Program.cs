66a9db77eec1a5bf1b88e97d33bc25fe333f74c0
Jackpieking <ledinhdangkhoa10a9@gmail.com>
2023-08-21 18:40:58 +0700

    Code ve bat dong bo trong c#

---
+++ DI/DIProj/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DIProj;
+
+public class Program
+{
+    /**
+     * Lập trình bất đồng bộ trong c# là đa luồng hoặc đơn luồng (tùy compiler)
+     *
+     * không thể biết trước thread nào thực hiện
+     *
+     * void => Task
+     * int, long, short => Task<datatype>
+     * 
+     * async => generate bunch of machine code
+     */
+
+    public static async Task Main(string[] args)
+    {
+        var task = HelloAsync();
+
+        Console.WriteLine("Do something else");
+
+        var res = await task;
+
+        Console.WriteLine($"Result: {res}");
+    }
+
+    public static async Task<string> HelloAsync()
+    {
+        await Task.Delay(1000);
+
+        return "Hello";
+    }
+}
